9b0d1a7c7898a5002091892f96b26f8433fafac3
alexraduca01 <alex.raduca01@icloud.com>
2023-10-13 16:21:47 +0200

    added snack 2

---
+++ array_tail/js/script.js
@@ -0,0 +1,39 @@
+// Creiamo un array chiedendo all'utente quanti elementi dovrà contenere e generando tanti numeri casuali da 1 a 100 per quanti sono gli elementi da inserire.
+// Stampiamo poi gli ultimi 5 elementi dell'Array
+// Bonus:
+// Chiediamo all'utente quanti elementi dell'array dovremo stampare
+// Suggerimenti:
+// Probabilmente conviene controllare che il numero di elementi della coda non sia più grande del numero di elementi totali dell'array
+
+function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) ) + min;
+}
+
+const input2 = document.getElementById('testo2');
+const input = document.getElementById('testo');
+const btn = document.querySelector('.btn-success');
+const btn2 = document.querySelector('.btn-danger');
+const result = document.querySelector('.result');
+const result2 = document.querySelector('.result2');
+const userNumberList = [];
+
+btn.addEventListener('click', function(){
+    
+    let rndNumber;
+
+    for(let a = 0; a < parseInt(input.value); a++){
+       
+        rndNumber = getRndInteger(1, 100);
+        userNumberList.push(rndNumber);
+    
+    }
+    
+    result.innerHTML = userNumberList.slice(userNumberList.length - 5, userNumberList.length);
+
+})
+
+btn2.addEventListener('click', function(){
+    
+    result2.innerHTML = userNumberList.slice((userNumberList.length - input2.value), userNumberList.length);
+
+})
